e555586f6bf210b314a5e56c00ac8c06249c2934
AndreyTitov18 <56290734+AndreyTitov18@users.noreply.github.com>
2019-11-05 10:00:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+<h1>Титов Андрей</h1>
+<h2>ИУ4-11Б</h2>
+<h3>Вариант-3</h3>
+<p> Задание 1</p>
+<?php
+for($i=0;$i<10;$i=1*($i+1))
+echo "$i . 1*($i+1);"
+for($j=0;$j<10;$j=2*($j+1);)
+echo "$j . 2*($j+1)";
+for($m=0;$m<10;$m=3*($m+1);)
+echo "$m . 3*($m+1)";
+for($k=0;$k<10;$k=4*($k+1);)
+echo "$k . 4*($k+1)";
+for($d=0;$d<10;$d=5*($d+1);)
+echo "$d . 5*($d+1)";
+for($u=0;$u<10;$u=6*($u+1);)
+echo "$u . 6*($u+1)";
+for($p=0;$p<10;$p=7*($p+1);)
+echo "$p . 7*($p+1)";
+for($l=0;$l<10;$l=8*($l+1);)
+echo "$l .8*($l+1)";
+for($t=0;$t<10;$t=9*($t+1);)
+echo "$t . 9*($t+1)";
+for($f=0;$f<10;$f=10*($f+1);)
+echo "$f . 10*($f+1)";
+
+?>
+<p>Задание 2</p>
+<p><input type="textarea" value="text">text</p>
+<select operation
+</body>
+
+
+</html>
